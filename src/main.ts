d952935930c557696bcdea9e97a8b8b469e571dd
ZombieBrand <zombiebrandg@gmail.com>
2021-12-16 22:52:19 +0800

    init

---
+++ src/main.ts
@@ -0,0 +1,8 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+// 通用字体
+import 'vfonts/Lato.css'
+// 等宽字体
+import 'vfonts/FiraCode.css'
+import naive from "./naive-ui";
+createApp(App).use(naive).mount('#app')
---
+++ src/naive-ui.ts
@@ -0,0 +1,10 @@
+import {
+    // create naive ui
+    create,
+    // component
+    NButton
+} from 'naive-ui'
+
+export default create({
+    components: [NButton]
+})
